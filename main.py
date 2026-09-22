ba5bf97c58f62f68f81ed0ba286ebcea10193938
lucasgesini45 <lucasgesini45@gmail.com>
2026-09-21 21:24:27 -0300

    Adiciona estrutura inicial de pastas

--- main.py
+++ main.py
@@ -5,9 +5,41 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
 @app.get("/")
-def teste():
+def home():
     return {
-        "status": "online",
         "mensagem": "EV ChargeOps API funcionando"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "online"
+    }
+
+
+@app.get("/carregadores")
+def listar_carregadores():
+    return {
+        "carregadores": []
+    }
+
+
+@app.get("/sessoes")
+def listar_sessoes():
+    return {
+        "sessoes": []
+    }
+
+
+@app.get("/consumo/calcular")
+def calcular_recarga(kwh: float, tarifa: float):
+    valor = kwh * tarifa
+
+    return {
+        "consumo_kwh": kwh,
+        "tarifa": tarifa,
+        "valor_total": round(valor, 2)
     }
